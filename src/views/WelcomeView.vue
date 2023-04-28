98d2cb637e182a9075e34f233f9de32f5e71ee9c
eimwe <prokopenkoserge@inbox.ru>
2023-04-28 16:57:54 +0600

    Project boilerplate

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">Welcome</div>
+</template>
+
+<style>
+.container {
+  max-width: 1200px;
+  margin-inline: auto;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: '/chatroom/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    devSourcemap: true
+  },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'images';
+          } else if (/woff2?|ttf|eot/i.test(extType)) {
+            extType = 'fonts';
+          } else if (/css/i.test(extType)) {
+            extType = 'styles';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/scripts/[name]-[hash].js',
+        entryFileNames: 'assets/scripts/[name]-[hash].js'
+      }
+    }
+  }
+});
