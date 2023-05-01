8cf16ca0398fcfbe03843b06df773e0fbed58cd9
eimwe <prokopenkoserge@inbox.ru>
2023-05-01 13:18:56 +0600

    Configured Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, serverTimestamp } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCRULrKR6bKqGE9aUGLR5cmysfX3MksfOU',
+  authDomain: 'chatroom-cf528.firebaseapp.com',
+  projectId: 'chatroom-cf528',
+  storageBucket: 'chatroom-cf528.appspot.com',
+  messagingSenderId: '529421422414',
+  appId: '1:529421422414:web:cd531e2f42e7fd67c0181f'
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const timestamp = serverTimestamp();
+
+export { db, timestamp };
