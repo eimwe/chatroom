580fe8703df2baf43252b06108dc8b8498d453dc
eimwe <prokopenkoserge@inbox.ru>
2023-05-03 13:49:31 +0600

    Updated authentication forms
    LoginForm.vue + Register.vue:
    1. Added required attribute to the inputs;
    2. Linked submit buttons and forms;
    3. Added blank method to handle each form submit.

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -6,13 +6,19 @@ import { UserCircleIcon, EyeIcon, EyeSlashIcon } from '@heroicons/vue/24/outline
 const username = ref('');
 const password = ref('');
 const hiddenPassword = ref(true);
+
+const signUserIn = () => {
+  console.log(`form submitted with Username: ${username.value}, Password: ${password.value}`);
+  username.value = '';
+  password.value = '';
+};
 </script>
 
 <template>
   <DialogTitle as="h3" class="text-center text-lg font-medium leading-6 text-gray-900">
     Sign in
   </DialogTitle>
-  <form class="mx-auto w-4/5">
+  <form id="signin-form" method="POST" @submit.prevent="signUserIn" class="mx-auto w-4/5">
     <div class="relative mt-2 rounded-md shadow-sm">
       <UserCircleIcon
         class="pointer-events-none absolute inset-y-0 left-0 top-1.5 flex h-6 w-6 items-center stroke-gray-400 pl-1"
@@ -22,6 +28,7 @@ const hiddenPassword = ref(true);
         type="text"
         name="username"
         id="username"
+        required
         class="block w-full rounded-md border-0 py-1.5 pl-7 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-900 sm:text-sm sm:leading-6"
         placeholder="Your login"
       />
@@ -47,6 +54,7 @@ const hiddenPassword = ref(true);
         :type="hiddenPassword ? 'password' : 'text'"
         name="password"
         id="password"
+        required
         class="block w-full rounded-md border-0 py-1.5 pl-7 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-900 sm:text-sm sm:leading-6"
         placeholder="Password"
       />
@@ -55,7 +63,8 @@ const hiddenPassword = ref(true);
 
   <div class="mt-4 text-center">
     <button
-      type="button"
+      type="submit"
+      form="signin-form"
       class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
     >
       Sign in
--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -7,13 +7,22 @@ const username = ref('');
 const email = ref('');
 const password = ref('');
 const hiddenPassword = ref(true);
+
+const signUserUp = () => {
+  console.log(
+    `form submitted with Username: ${username.value}, Password: ${password.value}, E-mail: ${email.value}`
+  );
+  username.value = '';
+  email.value = '';
+  password.value = '';
+};
 </script>
 
 <template>
   <DialogTitle as="h3" class="text-center text-lg font-medium leading-6 text-gray-900">
     Sign up
   </DialogTitle>
-  <form class="mx-auto w-4/5">
+  <form id="signup-form" method="POST" @submit.prevent="signUserUp" class="mx-auto w-4/5">
     <div class="relative mt-2 rounded-md shadow-sm">
       <UserCircleIcon
         class="pointer-events-none absolute inset-y-0 left-0 top-1.5 flex h-6 w-6 items-center stroke-gray-400 pl-1"
@@ -23,6 +32,7 @@ const hiddenPassword = ref(true);
         type="text"
         name="username"
         id="username"
+        required
         class="block w-full rounded-md border-0 py-1.5 pl-7 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-900 sm:text-sm sm:leading-6"
         placeholder="Your login"
       />
@@ -36,6 +46,7 @@ const hiddenPassword = ref(true);
         type="email"
         name="email"
         id="email"
+        required
         class="block w-full rounded-md border-0 py-1.5 pl-7 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-900 sm:text-sm sm:leading-6"
         placeholder="E-mail"
       />
@@ -61,6 +72,7 @@ const hiddenPassword = ref(true);
         :type="hiddenPassword ? 'password' : 'text'"
         name="password"
         id="password"
+        required
         class="block w-full rounded-md border-0 py-1.5 pl-7 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-900 sm:text-sm sm:leading-6"
         placeholder="Password"
       />
@@ -69,7 +81,8 @@ const hiddenPassword = ref(true);
 
   <div class="mt-4 text-center">
     <button
-      type="button"
+      type="submit"
+      form="signup-form"
       class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
     >
       Sign up
