39e3ae2002b855d860eec088485973e380033b34
eimwe <prokopenkoserge@inbox.ru>
2023-05-03 12:28:08 +0600

    Added show/hide password toggle

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { ref } from 'vue';
 import { DialogTitle } from '@headlessui/vue';
-import { UserCircleIcon, EyeIcon } from '@heroicons/vue/24/outline';
+import { UserCircleIcon, EyeIcon, EyeSlashIcon } from '@heroicons/vue/24/outline';
 
 const username = ref('');
 const password = ref('');
+const hiddenPassword = ref(true);
 </script>
 
 <template>
@@ -26,12 +27,24 @@ const password = ref('');
       />
     </div>
     <div class="relative mt-2 rounded-md shadow-sm">
+      <input
+        v-model="hiddenPassword"
+        name="toggle-password"
+        type="checkbox"
+        :checked="hiddenPassword"
+        class="absolute left-1.5 top-2.5 h-4 w-4 cursor-pointer rounded border-transparent text-transparent focus:ring-transparent"
+      />
+      <EyeSlashIcon
+        v-if="hiddenPassword"
+        class="pointer-events-none absolute inset-y-0 left-0 top-1.5 flex h-6 w-6 items-center stroke-gray-400 pl-1"
+      />
       <EyeIcon
+        v-else
         class="pointer-events-none absolute inset-y-0 left-0 top-1.5 flex h-6 w-6 items-center stroke-gray-400 pl-1"
       />
       <input
         v-model="password"
-        type="password"
+        :type="hiddenPassword ? 'password' : 'text'"
         name="password"
         id="password"
         class="block w-full rounded-md border-0 py-1.5 pl-7 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-900 sm:text-sm sm:leading-6"
