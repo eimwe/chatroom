5038ed5ed1759b47246f1d6e46d05100225d0ca0
eimwe <prokopenkoserge@inbox.ru>
2023-05-08 11:45:09 +0600

    Updated auth forms with extracted password input component

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,11 +1,15 @@
 <script setup>
 import { ref } from 'vue';
 import { DialogTitle } from '@headlessui/vue';
-import { AtSymbolIcon, EyeIcon, EyeSlashIcon } from '@heroicons/vue/24/outline';
+import { AtSymbolIcon } from '@heroicons/vue/24/outline';
+import InputPassword from './InputPassword.vue';
 
 const email = ref('');
 const password = ref('');
-const hiddenPassword = ref(true);
+
+const setPassword = (updatedPassword) => {
+  password.value = updatedPassword;
+};
 
 const signUserIn = () => {
   console.log(`form submitted with E-mail: ${email.value}, Password: ${password.value}`);
@@ -33,32 +37,7 @@ const signUserIn = () => {
         placeholder="E-mail"
       />
     </div>
-    <div class="relative mt-2 rounded-md shadow-sm">
-      <input
-        v-model="hiddenPassword"
-        name="toggle-password"
-        type="checkbox"
-        :checked="hiddenPassword"
-        class="absolute left-1.5 top-2.5 h-4 w-4 cursor-pointer rounded border-transparent text-transparent focus:ring-transparent"
-      />
-      <EyeSlashIcon
-        v-if="hiddenPassword"
-        class="pointer-events-none absolute inset-y-0 left-0 top-1.5 flex h-6 w-6 items-center stroke-gray-400 pl-1"
-      />
-      <EyeIcon
-        v-else
-        class="pointer-events-none absolute inset-y-0 left-0 top-1.5 flex h-6 w-6 items-center stroke-gray-400 pl-1"
-      />
-      <input
-        v-model="password"
-        :type="hiddenPassword ? 'password' : 'text'"
-        name="password"
-        id="password"
-        required
-        class="block w-full rounded-md border-0 py-1.5 pl-7 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-900 sm:text-sm sm:leading-6"
-        placeholder="Password"
-      />
-    </div>
+    <InputPassword @updatedPassword="setPassword" />
   </form>
 
   <div class="mt-4 text-center">
