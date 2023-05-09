d8c7dc07d5810be048282234166e9b95fdf5d8df
eimwe <prokopenkoserge@inbox.ru>
2023-05-09 15:06:31 +0600

    Added user area UI

---
+++ src/components/UserArea.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue';
+import { ArrowLeftOnRectangleIcon } from '@heroicons/vue/20/solid';
+</script>
+
+<template>
+  <Menu as="div" class="mt-1 inline-block sm:mt-0">
+    <MenuButton
+      class="inline-flex w-full items-center justify-center rounded-full bg-blue-200 bg-opacity-20 text-left hover:bg-opacity-30 focus:outline-none focus:ring-offset-2 focus:ring-offset-gray-800 focus-visible:ring-2 focus-visible:ring-blue-500 sm:gap-2 sm:rounded-md sm:px-2 sm:py-1"
+    >
+      <img
+        class="h-10 w-10 rounded-full"
+        src="https://tailwindui.com/img/avatar-1.jpg"
+        alt="Display name"
+      />
+      <div class="hidden text-sm text-white sm:block">
+        <p class="font-medium">Display name</p>
+        <span class="text-xs text-gray-200">e-mail</span>
+      </div>
+    </MenuButton>
+
+    <transition
+      enter-active-class="transition duration-100 ease-out"
+      enter-from-class="transform scale-95 opacity-0"
+      enter-to-class="transform scale-100 opacity-100"
+      leave-active-class="transition duration-75 ease-in"
+      leave-from-class="transform scale-100 opacity-100"
+      leave-to-class="transform scale-95 opacity-0"
+    >
+      <MenuItems
+        class="absolute right-0 mt-2 w-44 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:w-56"
+      >
+        <div class="px-1 py-1">
+          <div class="mb-2 block px-2 py-2 text-sm text-gray-900 sm:hidden">
+            <p class="font-medium">Display name</p>
+            <span class="text-xs text-gray-500">e-mail</span>
+          </div>
+          <MenuItem v-slot="{ active }">
+            <button
+              :class="[
+                active ? 'bg-blue-200 text-blue-900' : 'text-gray-900',
+                'group flex w-full items-center gap-1 rounded-md px-2 py-2 text-sm'
+              ]"
+            >
+              <ArrowLeftOnRectangleIcon class="h-4 w-4" />
+              Log out
+            </button>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </transition>
+  </Menu>
+</template>
