f99eb65391da137cd250fba2f87f530eb2a115b6
eimwe <prokopenkoserge@inbox.ru>
2023-05-09 16:41:16 +0600

    Redirected logged out user to the Homepage

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,6 +1,13 @@
 <script setup>
+import { useRouter } from 'vue-router';
 import logoImage from '../assets/images/logo.png';
 import UserArea from './UserArea.vue';
+
+const router = useRouter();
+
+const returnHome = () => {
+  router.push({ path: '/' });
+};
 </script>
 
 <template>
@@ -11,7 +18,7 @@ import UserArea from './UserArea.vue';
           <img class="h-8 w-auto" :src="logoImage" alt="Chatroom" />
         </div>
         <div class="flex items-center gap-3">
-          <UserArea />
+          <UserArea @signedOut="returnHome" />
         </div>
       </div>
     </nav>
