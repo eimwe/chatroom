bac0e93dc821cda75ef43ccfd91a3929d6978f60
eimwe <prokopenkoserge@inbox.ru>
2023-05-09 17:49:16 +0600

    Added firebase auth check before app mounting
    To prevent navigation guard from redirecting user to the Homepage on page reload.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { auth } from './firebase/config.js';
+import { onAuthStateChanged } from 'firebase/auth';
 
 import './assets/main.css';
 
-const app = createApp(App);
+let app;
 
-app.use(router);
-
-app.mount('#app');
+onAuthStateChanged(auth, () => {
+  if (!app) {
+    app = createApp(App).use(router).mount('#app');
+  }
+});
