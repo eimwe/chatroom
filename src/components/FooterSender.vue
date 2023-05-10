ac77af9f908eab07f02c396bc2c731b9d7048ee3
eimwe <prokopenkoserge@inbox.ru>
2023-05-10 15:06:26 +0600

    Created footer and imported message form inside

---
+++ src/components/FooterSender.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MessageForm from '../components/MessageForm.vue';
+</script>
+
+<template>
+  <footer class="fixed bottom-0 left-0 right-0">
+    <div class="mx-auto max-w-7xl px-4 py-4 sm:px-6 sm:py-6 lg:px-8 lg:py-8">
+      <MessageForm />
+    </div>
+  </footer>
+</template>
