c38cdaaa390729eab228c0341fc57fd7f5c76fa8
eimwe <prokopenkoserge@inbox.ru>
2023-05-10 15:06:59 +0600

    Imported footer to the chat page

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import HeaderNav from '../components/HeaderNav.vue';
+import FooterSender from '../components/FooterSender.vue';
 </script>
 
 <template>
   <HeaderNav />
+  <FooterSender />
 </template>
