2d26af4b90650774de489fa77e19c1812f280750
eimwe <prokopenkoserge@inbox.ru>
2023-05-12 18:08:19 +0600

    Created chat window template
    Rendered all messages from firestore database.

---
+++ src/components/ChatBox.vue
@@ -0,0 +1,18 @@
+<script setup>
+import getCollection from '../composables/getCollection.js';
+
+const { messages, error } = getCollection('messages');
+</script>
+
+<template>
+  <main>
+    <div v-if="messages" class="mx-auto max-w-7xl px-4 py-4 sm:px-6 sm:py-6 lg:px-8 lg:py-8">
+      <div v-if="error">{{ error }}</div>
+      <figure v-for="reply in messages" :key="reply.id">
+        <blockquote>{{ reply.message }}</blockquote>
+        <figcaption>{{ reply.name }}</figcaption>
+        <span>{{ reply.createdAt.toDate() }}</span>
+      </figure>
+    </div>
+  </main>
+</template>
