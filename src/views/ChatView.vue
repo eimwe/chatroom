eccc9ef675ada6904015dd4f20070fc89f1150ac
eimwe <prokopenkoserge@inbox.ru>
2023-05-12 18:10:09 +0600

    Imported chat window to the chat page

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HeaderNav from '../components/HeaderNav.vue';
 import FooterSender from '../components/FooterSender.vue';
+import ChatBox from '../components/ChatBox.vue';
 </script>
 
 <template>
   <HeaderNav />
+  <ChatBox />
   <FooterSender />
 </template>
