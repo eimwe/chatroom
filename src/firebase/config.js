1bbe62fb9cab969e8cc0dad50a3cd20dcd3ba7de
eimwe <prokopenkoserge@inbox.ru>
2023-05-14 15:54:39 +0600

    Refactored firebase config file
    Deleted unused timestamp import.

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, serverTimestamp } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
@@ -13,7 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const timestamp = serverTimestamp();
 const auth = getAuth(app);
 
-export { db, timestamp, auth };
+export { db, auth };
