3bac99a2f8a9b2a499b8c32d78f72cdf5e76d310
eimwe <prokopenkoserge@inbox.ru>
2023-05-15 11:14:59 +0600

    Retrieved hh:mm from date to display in a message

--- src/components/ChatBox.vue
+++ src/components/ChatBox.vue
@@ -1,17 +1,29 @@
 <script setup>
+import { computed } from 'vue';
 import getCollection from '../composables/getCollection.js';
 
 const { messages, error } = getCollection('messages');
+
+const formattedMessages = computed(() => {
+  return messages.value.map((reply) => {
+    const date = reply.createdAt.toDate();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    if (minutes < 10) minutes = `0${minutes}`;
+    let time = `${hours}:${minutes}`;
+    return { ...reply, createdAt: time };
+  });
+});
 </script>
 
 <template>
   <main>
     <div v-if="messages" class="mx-auto max-w-7xl px-4 py-4 sm:px-6 sm:py-6 lg:px-8 lg:py-8">
       <div v-if="error">{{ error }}</div>
-      <figure v-for="reply in messages" :key="reply.id">
+      <figure v-for="reply in formattedMessages" :key="reply.id">
         <blockquote>{{ reply.message }}</blockquote>
         <figcaption>{{ reply.name }}</figcaption>
-        <span>{{ reply.createdAt.toDate() }}</span>
+        <span>{{ reply.createdAt }}</span>
       </figure>
     </div>
   </main>
