b66f37049732507b43f05741986f5328ddd96f9e
eimwe <prokopenkoserge@inbox.ru>
2023-05-15 16:18:41 +0600

    Added auto-scrolling to the chat box
    The logic is contained in 'scrollToLastMessage' method which is triggered on each 'onUpdated' life cycle hook. Additional 'scroll-margin-bottom' properties were added to the message container as tailwindcss classes to align container with the footer.

--- src/components/ChatBox.vue
+++ src/components/ChatBox.vue
@@ -1,8 +1,10 @@
 <script setup>
-import { computed } from 'vue';
+import { ref, computed, onUpdated } from 'vue';
 import getCollection from '../composables/getCollection.js';
 
 const { messages, error } = getCollection('messages');
+
+const messageContainer = ref(null);
 
 const formattedMessages = computed(() => {
   return messages.value.map((reply) => {
@@ -14,11 +16,26 @@ const formattedMessages = computed(() => {
     return { ...reply, createdAt: time };
   });
 });
+
+const scrollToLastMessage = () => {
+  messageContainer.value?.scrollIntoView({
+    block: 'end',
+    behavior: 'smooth'
+  });
+};
+
+onUpdated(() => {
+  scrollToLastMessage();
+});
 </script>
 
 <template>
   <main class="min-h-[100dvh]">
-    <div v-if="messages" class="mx-auto max-w-7xl px-4 pt-4 sm:px-6 sm:pt-6 lg:px-8 lg:pt-8">
+    <div
+      v-if="messages"
+      ref="messageContainer"
+      class="mx-auto max-w-7xl scroll-mb-16 px-4 pt-4 sm:scroll-mb-28 sm:px-6 sm:pt-6 lg:scroll-mb-32 lg:px-8 lg:pt-8"
+    >
       <div v-if="error">{{ error }}</div>
       <figure v-for="reply in formattedMessages" :key="reply.id">
         <blockquote>{{ reply.message }}</blockquote>
