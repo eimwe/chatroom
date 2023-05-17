a475d2da527ab36ae2a4077f0797eb31f804d080
eimwe <prokopenkoserge@inbox.ru>
2023-05-17 16:15:16 +0600

    Aligned current user messages

--- src/components/ChatBox.vue
+++ src/components/ChatBox.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { ref, computed, onUpdated } from 'vue';
 import getCollection from '../composables/getCollection.js';
+import getUser from '../composables/getUser.js';
 
 const { messages, error } = getCollection('messages');
+const { user } = getUser();
 
 const messageContainer = ref(null);
 
@@ -24,6 +26,10 @@ const scrollToLastMessage = () => {
   });
 };
 
+const formatUserMessages = (currentUser, dataBaseUser) => {
+  return { 'place-self-end items-end': currentUser == dataBaseUser };
+};
+
 onUpdated(() => {
   scrollToLastMessage();
 });
@@ -37,8 +43,13 @@ onUpdated(() => {
       class="mx-auto max-w-7xl scroll-mb-16 px-4 pt-4 sm:scroll-mb-28 sm:px-6 sm:pt-6 lg:scroll-mb-32 lg:px-8 lg:pt-8"
     >
       <div v-if="error" class="text-center text-red-600">{{ error }}</div>
-      <div class="grid gap-2">
-        <figure v-for="reply in formattedMessages" :key="reply.id" class="flex flex-col-reverse">
+      <div v-if="user" class="grid gap-2">
+        <figure
+          v-for="reply in formattedMessages"
+          :key="reply.id"
+          class="flex flex-col-reverse"
+          :class="formatUserMessages(user.displayName, reply.name)"
+        >
           <blockquote class="w-fit rounded-md bg-blue-100 px-2 py-1 text-blue-900">
             {{ reply.message }}
           </blockquote>
