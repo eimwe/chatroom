cb3acf5697fa01d67d69752a599a7934daa4513e
eimwe <prokopenkoserge@inbox.ru>
2023-05-22 16:57:03 +0600

    Created separate avatar component
    Render random image as user avatar or a default icon in case of error.

---
+++ src/components/UserAvatar.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import getRandomImage from '../composables/getRandomImage.js';
+import { UserCircleIcon } from '@heroicons/vue/24/outline';
+import { url, options } from '../api/userAvatarConfig.js';
+
+const { getImageHref } = getRandomImage();
+const userAvatar = ref(null);
+
+onMounted(async () => {
+  userAvatar.value = await getImageHref(url, options);
+});
+</script>
+
+<template>
+  <img v-if="userAvatar" class="h-10 w-10 rounded-full" :src="userAvatar" />
+  <UserCircleIcon v-else class="h-10 w-10 text-gray-200" />
+</template>
