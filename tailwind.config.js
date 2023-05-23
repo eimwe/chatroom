6e42b076f03dd864cfd242bada80a9034218dac3
eimwe <prokopenkoserge@inbox.ru>
2023-05-23 14:11:57 +0600

    Configured tailwind to switch between light/dark modes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./**/*.html', './src/**/*.vue', './src/components/**/*.vue', './src/views/**/*.vue'],
   theme: {
     extend: {}
