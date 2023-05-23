520dc5af6aaf7c2af447a64dd7777a6a637fbdb3
eimwe <prokopenkoserge@inbox.ru>
2023-05-23 15:08:22 +0600

    Adjusted layout styles for dark/light mode

--- src/components/FooterSender.vue
+++ src/components/FooterSender.vue
@@ -3,7 +3,7 @@ import MessageForm from '../components/MessageForm.vue';
 </script>
 
 <template>
-  <footer class="sticky bottom-0 bg-cyan-900">
+  <footer class="sticky bottom-0 bg-blue-100 dark:bg-cyan-900">
     <div class="mx-auto max-w-4xl px-4 py-4 sm:px-8 sm:py-6 lg:px-0 lg:py-8">
       <MessageForm />
     </div>
