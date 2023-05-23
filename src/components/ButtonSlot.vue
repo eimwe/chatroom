80c5d9976098f88f486a2fc26439ecd35770d5c4
eimwe <prokopenkoserge@inbox.ru>
2023-05-23 15:57:07 +0600

    Created slotted button to reuse across the app

---
+++ src/components/ButtonSlot.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="inline-flex justify-center rounded-md border-2 border-blue-900 bg-transparent px-2 py-1 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 dark:border-transparent dark:bg-blue-100 dark:hover:bg-blue-200 sm:px-4 sm:py-2"
+  >
+    <slot>Ok</slot>
+  </button>
+</template>
