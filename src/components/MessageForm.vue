639fc05bd35290a3721a0ada6621ee2f5b293909
eimwe <prokopenkoserge@inbox.ru>
2023-05-24 10:55:25 +0600

    Fixed Firefox textarea bug
    Eliminated an extra row by adding 'overflow-x: hidden' rule.

--- src/components/MessageForm.vue
+++ src/components/MessageForm.vue
@@ -43,7 +43,7 @@ const sendMessage = async () => {
         name="message"
         rows="1"
         placeholder="Type message here"
-        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:py-5 sm:text-sm sm:leading-6"
+        class="block w-full overflow-x-hidden rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:py-5 sm:text-sm sm:leading-6"
       ></textarea>
       <ButtonSlot type="submit">
         <span class="sr-only">Send</span>
